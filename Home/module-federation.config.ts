121eea19c7a756dc572ac5a7cbcee51ec7c22135
Elijah Prince <elijahprince73@gmail.com>
2026-01-19 14:31:03 -0800

    Micro Front End with Vite

--- Home/module-federation.config.ts
+++
@@ -1,9 +0,0 @@
-export const mfConfig = {
-  name: "Home",
-  filename: "remoteEntry.js",
-  exposes: {
-    "./Header": "./src/Header",
-    "./Footer": "./src/Footer",
-  },
-  shared: ["react", "react-dom"],
-};
--- Products/module-federation.config.ts
+++
@@ -1,9 +0,0 @@
-export const mfConfig = {
-  name: "Products",
-  exposes: {},
-  shared: ["react", "react-dom"],
-  remotes: {
-    home: "home@http://localhost:3000/remoteEntry.js",
-  },
-  filename: "remoteEntry.js",
-};
